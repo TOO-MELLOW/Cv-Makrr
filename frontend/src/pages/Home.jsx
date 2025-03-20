8630b76db0c6aa5175bfb210d0d0f43bd83f64c4
TOO-MELLOW <95143661+TOO-MELLOW@users.noreply.github.com>
2025-03-21 00:11:34 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,15 @@
-v
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import TemplateGallery from '../components/TemplateGallery';
+
+const Home = () => (
+  <div className="min-h-screen flex flex-col">
+    <Header />
+    <main className="flex-grow p-4">
+      <TemplateGallery />
+    </main>
+    <Footer />
+  </div>
+);
+
+export default Home;
